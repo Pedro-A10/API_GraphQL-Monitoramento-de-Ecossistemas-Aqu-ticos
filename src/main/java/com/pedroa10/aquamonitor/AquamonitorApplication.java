8e6ac1c9dea9135335c71f33395f843838cec161
Pedro-A10 <pedroaugusto732@gmail.com>
2025-05-24 14:20:26 -0300

    Resolve conflito de .gitignore e configuração do banco de dados

---
+++ src/main/java/com/pedroa10/aquamonitor/AquamonitorApplication.java
@@ -0,0 +1,13 @@
+package com.pedroa10.aquamonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquamonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquamonitorApplication.class, args);
+	}
+
+}
