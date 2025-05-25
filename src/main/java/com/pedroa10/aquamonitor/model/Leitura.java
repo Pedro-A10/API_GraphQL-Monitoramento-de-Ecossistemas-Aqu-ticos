43e16e1def5da5263bea1290f95c4fb266070bea
Pedro-A10 <pedroaugusto732@gmail.com>
2025-05-24 21:15:25 -0300

    Criação da classe Leitura

---
+++ src/main/java/com/pedroa10/aquamonitor/model/Leitura.java
@@ -0,0 +1,65 @@
+package com.pedroa10.aquamonitor.model;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Leitura {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private double valor;
+	
+	@Column(columnDefinition = "TIMESTAMP")
+	private Instant timestap;
+	private boolean anomalia;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sensor_id")
+	private Sensor sensor;
+	
+	public Leitura(double valor, Sensor sensor) {
+		this.valor = valor;
+		this.sensor = sensor;
+		this.timestap = Instant.now();	
+	}
+	
+	public boolean validarValor() {
+		return switch (sensor.getTipo()) {
+			case PH -> valor >= 0 && valor <= 14;
+			case OXIGENIO -> valor >= 0;
+			case TEMPERATURA -> valor >= -50 && valor <= 100;
+		};
+	}
+	
+	public boolean detectarAnomalia() {
+		boolean isAnomalia = switch (sensor.getTipo()) {	
+			case PH -> valor < 6.5 || valor > 8.5;
+			case OXIGENIO -> valor < 5.0;
+			case TEMPERATURA -> valor > 30;
+		};
+		this.anomalia = isAnomalia;
+		return isAnomalia;
+	}
+	
+	public ZonedDateTime getTimestampAsZonedDateTime(ZoneId zoneId) {
+		return timestap.atZone(zoneId);
+	}
+}
