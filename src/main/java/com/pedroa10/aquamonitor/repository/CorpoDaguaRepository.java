4871cf40fe8b8f9caa5652794a3fa814cb768b26
Pedro-A10 <pedroaugusto732@gmail.com>
2025-05-31 09:23:04 -0300

    Renomeação de interface e pacote

--- src/main/java/com/pedroa10/aquamonitor/repository/CorpoDaguaRepository.java
+++ src/main/java/com/pedroa10/aquamonitor/repository/CorpoDaguaRepository.java
@@ -1,4 +1,4 @@
-package com.pedroa10.aquamonitorl.Repository;
+package com.pedroa10.aquamonitor.repository;
 
 import java.util.List;
 
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CorpoDagua extends JpaRepository<CorpoDagua, Long> {
+public interface CorpoDaguaRepository extends JpaRepository<CorpoDaguaRepository, Long> {
 
-	List<CorpoDagua> findByTipo(String tipo);
+	List<CorpoDaguaRepository> findByTipo(String tipo);
 	
 	@Query("SELECT c FROM CorpoDagua c WHERE c.localizacao.latitude BETWEEN :minLat AND :maxLat AND c.localizacao.longitude BETWEEN :minLon AND :maxLon")
-	List<CorpoDagua> findByLocalizacaoRange(
+	List<CorpoDaguaRepository> findByLocalizacaoRange(
 		@Param("minLat") double minLat,
 		@Param("maxLat") double maxLat,
 		@Param("minLon") double minLon,
