dc62f93495f21be44640c82e0e9c633a04bfa0b7
Pedro-A10 <pedroaugusto732@gmail.com>
2025-06-04 16:35:33 -0300

    Ajuste na interface AlertaService

--- src/main/java/com/pedroa10/aquamonitor/service/AlertaService.java
+++ src/main/java/com/pedroa10/aquamonitor/service/AlertaService.java
@@ -1,5 +1,15 @@
 package com.pedroa10.aquamonitor.service;
 
+import java.util.List;
+
+import com.pedroa10.aquamonitor.model.Alerta;
+import com.pedroa10.aquamonitor.model.Leitura;
+
 public interface AlertaService {
 
+	Alerta salvarAlerta(Alerta alerta);
+	List<Alerta> buscarAlertasNaoResolvidos();
+	Void marcarComoResolvido(Long AlertaId);
+	List<Alerta> buscarPorPrioridade(String prioridade);
+	Alerta gerarAlertaAutomatico(Leitura leituraAlomalia);
 }
